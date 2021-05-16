29892dddead69168ccc1515ba19ef0c0c0f1cfbe
Davve_DP <davve146@gmail.com>
2021-05-16 22:58:40 +0200

    Added all extras

--- README.md
+++ README.md
@@ -1,8 +1,19 @@
 # Waza-Editor
-A move effect editor for the generation 4 Pokemon games
+A move effect editor for the generation 4 Pokemon games. Originally developed by Acent (all rights reserved), now with several added features like:
+
+1. Support for HGSS roms (External RomDatabase XML file for user-specific rom details).
+2. Soon to include Animation Database Library to view all in-game animations in real-time (downloads from Bulbapedia).
+3. Deals directly with rom files instead of extracted DSPRE/SDMSE folders.
+
+Credits also to @murrple-1 for APNG viewer for displaying animated PNGs in winforms.
+
+**DISCLAIMER** This is project is still a work in progress, full functionality is not guaranteed! (gitignore will also be configured later)
 
 ![Demo image](demo.png)
 
 ## Installation
-Build using Visual Studio, requires version 2019 (HTML Agility Pack NuGet required for modifying source)
+Build using Visual Studio, requires version 2019 (HTML Agility Pack NuGet required)
+
+Releases will be supplied in later releases.
+
 If any problems should arise feel free to send a dm on discord @DavveDP#6813
--- WEditor/About.Designer.cs
+++ WEditor/About.Designer.cs
@@ -43,9 +43,10 @@
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("MV Boli", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.Color.RoyalBlue;
-            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Location = new System.Drawing.Point(-2, 8);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(522, 79);
+            this.label1.Size = new System.Drawing.Size(414, 63);
             this.label1.TabIndex = 0;
             this.label1.Text = "WazaEffectEditor";
             // 
@@ -53,18 +54,20 @@
             // 
             this.label2.AutoSize = true;
             this.label2.ForeColor = System.Drawing.Color.RoyalBlue;
-            this.label2.Location = new System.Drawing.Point(26, 92);
+            this.label2.Location = new System.Drawing.Point(17, 71);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(194, 90);
+            this.label2.Size = new System.Drawing.Size(160, 78);
             this.label2.TabIndex = 1;
-            this.label2.Text = "For Platinum\r\nStart : 2020-10-24\r\nBeta ver finish : 2020-12-19\r\n1.0 : 2021-3-18\r\n" +
-    "Update : Soon!\r\nCreated by Acent\r\n";
+            this.label2.Text = "For Gen 4\r\nStart : 2020-10-24\r\nBeta ver 1.1 finish : 2021-05-16\r\n1.1 : 2021-3-18\r" +
+    "\nUpdate : Soon!\r\nCreated by Acent and DavveDP";
             // 
             // btnSaveCurrent
             // 
-            this.btnSaveCurrent.Location = new System.Drawing.Point(459, 232);
+            this.btnSaveCurrent.Location = new System.Drawing.Point(344, 201);
+            this.btnSaveCurrent.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
             this.btnSaveCurrent.Name = "btnSaveCurrent";
-            this.btnSaveCurrent.Size = new System.Drawing.Size(75, 23);
+            this.btnSaveCurrent.Size = new System.Drawing.Size(56, 20);
             this.btnSaveCurrent.TabIndex = 2;
             this.btnSaveCurrent.Text = "OK";
             this.btnSaveCurrent.UseVisualStyleBackColor = true;
@@ -73,9 +76,10 @@
             // linkLabel1
             // 
             this.linkLabel1.AutoSize = true;
-            this.linkLabel1.Location = new System.Drawing.Point(3, 0);
+            this.linkLabel1.Location = new System.Drawing.Point(2, 0);
+            this.linkLabel1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.linkLabel1.Name = "linkLabel1";
-            this.linkLabel1.Size = new System.Drawing.Size(481, 15);
+            this.linkLabel1.Size = new System.Drawing.Size(363, 13);
             this.linkLabel1.TabIndex = 3;
             this.linkLabel1.TabStop = true;
             this.linkLabel1.Text = "Mero Mero\'s effect list : https://www.youtube.com/watch?v=gukPRu2iZ_w";
@@ -84,9 +88,10 @@
             // linkLabel2
             // 
             this.linkLabel2.AutoSize = true;
-            this.linkLabel2.Location = new System.Drawing.Point(3, 15);
+            this.linkLabel2.Location = new System.Drawing.Point(2, 13);
+            this.linkLabel2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.linkLabel2.Name = "linkLabel2";
-            this.linkLabel2.Size = new System.Drawing.Size(447, 15);
+            this.linkLabel2.Size = new System.Drawing.Size(346, 13);
             this.linkLabel2.TabIndex = 4;
             this.linkLabel2.TabStop = true;
             this.linkLabel2.Text = "Or buibapedia : https://bulbapedia.bulbagarden.net/wiki/List_of_moves";
@@ -96,21 +101,23 @@
             // 
             this.panel1.Controls.Add(this.linkLabel1);
             this.panel1.Controls.Add(this.linkLabel2);
-            this.panel1.Location = new System.Drawing.Point(26, 188);
+            this.panel1.Location = new System.Drawing.Point(20, 163);
+            this.panel1.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(487, 38);
+            this.panel1.Size = new System.Drawing.Size(365, 33);
             this.panel1.TabIndex = 5;
             // 
             // About
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(552, 267);
+            this.ClientSize = new System.Drawing.Size(414, 231);
             this.Controls.Add(this.panel1);
             this.Controls.Add(this.btnSaveCurrent);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
             this.Name = "About";
             this.Text = "About";
             this.panel1.ResumeLayout(false);
